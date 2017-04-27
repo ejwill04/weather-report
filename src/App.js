01c33a3dada363f0dd32badc2412d14907feeb03
Elijah Williams <ejwill04@gmail.com>
2017-04-27 15:51:19 -0600

    Set up input and submit skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ userInput: e.target.value });
+  }
+
+  handleSubmit() {
+    console.log('submit')
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Weather Report</h2>
+        </div>
+        <div id='search-container'>
+          <input
+              id='inputField'
+              placeholder='Search by City'
+              value={this.state.userInput}
+              onChange={this.handleChange}
+            ></input>
+          <button
+              id='submitBtn'
+              onClick={this.handleSubmit}
+            >Search
+          </button>
         </div>
       </div>
     );
